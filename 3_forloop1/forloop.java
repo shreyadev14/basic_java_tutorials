9e0c84b92fbe5ac1a8bb944fb4b7ed38bfed6d65
Shreya Dev <shreyadev14@gmail.com>
2018-02-02 01:59:15 +0530

    String.format() introduced.

--- 3_forloop1/forloop.java
+++ 3_forloop1/forloop.java
@@ -5,9 +5,10 @@ public static void main(String args[])
 int x,y;
 for (x=0;x<5;x++)
 {
- y=x*4;
- System.out.println("The value of y is" + y );
- System.out.println( "for x=" + x );
+ y=x*4; //assignment statement
+ //System.out.println("The value of y is" + y );
+ //System.out.println( "for x=" + x );
+System.out.println(String.format("The values are x=%d and y=%d",x,y));
  }
 } 
 }
